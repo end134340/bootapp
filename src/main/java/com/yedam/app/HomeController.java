233f15a36790ef15468a5d7957eb102ea684c58d
end134340 <end921@naver.com>
2025-08-11 12:31:29 +0900

    first commit

---
+++ src/main/java/com/yedam/app/HomeController.java
@@ -0,0 +1,16 @@
+package com.yedam.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	@ResponseBody
+	public String main() {
+		System.out.println("main handler 실행됨.");
+		return "hello springBoot";
+	}
+}
